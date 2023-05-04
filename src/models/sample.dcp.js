702475b7ad7ad5a38a3aaf944a919e43e7f4e644
Sergio Terroso <sergio.terroso@gmail.com>
2023-05-03 18:36:26 -0600

    Daily Coding Problem sample

--- src/models/sample.dcp.js
+++ src/models/sample.dcp.js
@@ -14,7 +14,7 @@
  * What if, instead of being able to climb 1 or 2 steps at a time, you could
  * climb any number from a set of positive integers X? For example, if
  * X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time. Generalize your
- * function to take in X. 
+ * function to take in X.
  *****************************************************************************/
 
 export const MAX_STEPS = 99999;
@@ -22,34 +22,34 @@ export const MAX_STEPS = 99999;
 /**
  * Calculates the number of combinations in which a staircase can be climbed,
  * given a max number of steps that can be climbed at a time (2 max by default).
- * 
+ *
  * @param {number} steps The number of steps for a staircase.
- * @param {number} max The max number of steps that can be climbed at a time.
+ * @param {{configurations?: number[]}} options The list of all possible
+ * number of steps that can be climbed at a time. By default is [1, 2]:
+ * one or two steps at a time.
  * @throws { Error } If the value provided for the _steps_ parameter is not
  * an integer number.
  * @throws { RangeError } If the value provided for the _steps_ parameter is
  * less than 0 or greater than MAX_STEPS.
- * @returns {number} the number of unique ways a staircase can be climbed, 
+ * @returns {number} the number of unique ways a staircase can be climbed,
  * given a max number of steps that can be climbed at a time.
  */
-export const UniqueWays = (steps, max = 2) => {
-    if (steps === 0) return 1;
-    
-    if (steps < 4) return steps;
+export const UniqueWays = (steps, options = { configurations: [1, 2] }) => {
+  if (steps === 0) return 1;
 
-    const combinations = [];
+  let combinations = [1];
 
-    for (let i = 1; i <= max; i++) {
-        for (let j = 1; j <= i; j++) {
-            let way = [];
+  for (let i = 1; i <= steps; i++) {
+    let stepSum = 0;
 
-            while (way.reduce((sum, item) => sum + item, 0) < steps) {
-                way.push(j);
-            }
+    options.configurations.forEach((configuration) => {
+      if (i - configuration >= 0) {
+        stepSum += combinations[i - configuration];
+      }
+    });
 
-            combinations.push(way);
-        }
-    }
+    combinations[i] = stepSum;
+  }
 
-    return combinations.length;
-}
+  return combinations[steps];
+};
